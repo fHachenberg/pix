f5b0b2cd06c5e7b67fcd26a2110a352138e91662
Tim Gerstner <timgerst@cs.rutgers.edu>
2013-04-22 10:04:04 -0400

    Added missing Resources.h file and two images used by the UI.
    Signed-off-by: Tim Gerstner <timgerst@cs.rutgers.edu>

---
+++ Resources.h
@@ -0,0 +1,33 @@
+/* 
+Copyright (c) 2013, Timothy Gerstner, All rights reserved.
+
+This code is part of the prototype C++ implementation of our paper/ my thesis.
+
+Public repository: https://github.com/timgerst/pix
+Project Webpage:  http://www.research.rutgers.edu/~timgerst/
+
+This code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this code.  If not, see <http://www.gnu.org/licenses/>.
+
+Author: Timothy Gerstner, timgerst@cs.rutgers.edu
+*/
+
+#pragma once
+#include "cinder\CinderResources.h"
+
+#define kMainViewLOCK			CINDER_RESOURCE( ../src/, lock.png, 128, IMAGE)
+#define kMainViewSELC			CINDER_RESOURCE( ../src/, selc_tex.png, 129, IMAGE)
+
+
+
+
